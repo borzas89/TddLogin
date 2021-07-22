2e66d3ffbc26cc3e230e2a913195a150a568616c
borzas89 <borza.zsolt89@gmail.com>
2021-07-22 13:44:47 +0200

    Update AuthenticationManager.kt
    adding clientId

--- app/src/main/java/example/com/tddlogin/data/AuthenticationManager.kt
+++ app/src/main/java/example/com/tddlogin/data/AuthenticationManager.kt
@@ -7,6 +7,8 @@ import javax.inject.Singleton
 @Singleton
 class AuthenticationManager @Inject constructor(val sharedPref: SharedPreferences) {
 
+    fun getClientId(): String  = "69bfdce9-2c9f-4a12-aa7b-4fe15e1228dc"
+
     fun isAuthenticated(): Boolean =
         sharedPref.getString("token", "")!!.isNotEmpty()
 
